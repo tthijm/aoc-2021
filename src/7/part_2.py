deff884cc561214ed27b86abbf888a30ecadba08
tthijm <59415467+tthijm@users.noreply.github.com>
2024-05-06 17:06:29 +0200

    Add day 7 solution

---
+++ src/7/part_2.py
@@ -0,0 +1,32 @@
+import sys, collections
+
+with open(sys.argv[1]) as f: lines = [l.strip() for l in f.readlines()]
+
+crabs = [int(x) for x in lines[0].split(",")]
+q = collections.deque([sum(crabs) // len(crabs)])
+res = 2**32
+dp = { 0:0, 1: 1 }
+
+def f(n):
+  if n in dp:
+    return dp[n]
+
+  v = n + f(n - 1)
+
+  dp[n] = v
+
+  return v
+
+while q:
+  v = q.popleft()
+  fuel = 0
+
+  for crab in crabs:
+    fuel += f(abs(v - crab))
+
+  if fuel < res:
+    res = fuel
+    q.append(v - 1)
+    q.append(v + 1)
+
+print(res)
