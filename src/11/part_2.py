573a546f7a61947a7e836f04498b2027d00e4df8
tthijm <59415467+tthijm@users.noreply.github.com>
2024-05-10 21:50:40 +0200

    Add day 11 solution

---
+++ src/11/part_2.py
@@ -0,0 +1,41 @@
+import sys
+
+with open(sys.argv[1]) as f: lines = [l.strip() for l in f.readlines()]
+
+DS = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+lines = [[int(x) for x in l] for l in lines]
+res = 0
+
+def f(x, y):
+  lines[y][x] = -1
+
+  for dx, dy in DS:
+    nx, ny = x + dx, y + dy
+
+    if 0 <= nx < len(lines[0]) and 0 <= ny < len(lines) and lines[ny][nx] != -1:
+      lines[ny][nx] += 1
+
+      if lines[ny][nx] > 9:
+        f(nx, ny)
+
+for i in range(10**10):
+  lines = [[x + 1 for x in l] for l in lines]
+  done = True
+
+  for y in range(len(lines)):
+    for x in range(len(lines[0])):
+      if lines[y][x] > 9:
+        f(x, y)
+
+  for y in range(len(lines)):
+    for x in range(len(lines[0])):
+      if lines[y][x] == -1:
+        res += 1
+        lines[y][x] = 0
+      else:
+        done = False
+  
+  if done:
+    break
+
+print(i + 1)
